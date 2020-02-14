7f276f1e9159a3836e71741395d9bd3904801769
SinLapis <stradust0001@gmail.com>
2020-02-14 09:54:24 +0800

    绕过moshi不支持ArrayList

--- src/main/kotlin/org/ndp/dns_query_rr/bean/DNSRR.kt
+++ src/main/kotlin/org/ndp/dns_query_rr/bean/DNSRR.kt
@@ -3,6 +3,6 @@ package org.ndp.dns_query_rr.bean
 data class DNSRR(
     val domain: String,
     val dnsServer: String,
-    val aRecord: ArrayList<String>,
-    val cName: ArrayList<String>
+    val aRecord: List<String>,
+    val cName: List<String>
 )
